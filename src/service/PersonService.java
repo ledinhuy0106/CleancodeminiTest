85b07d368d91416e66421807599d1c57c4691be1
uyle1234 <linhconuong2k3@gmail.com>
2021-10-21 14:30:34 +0700

    add

---
+++ src/service/PersonService.java
@@ -0,0 +1,18 @@
+package service;
+
+public interface PersonService<T> {
+    int findIndexByCode(int code);
+
+    void add(T t);
+
+    void update(T t, int code);
+
+    void deleteByCode(int code);
+
+    void print();
+
+    void sort();
+
+    double sumScore();
+
+}
